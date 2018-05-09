3e8d9e86d07192ee85a273515ab03625c78e9eb9
phenom_faiz_ansari <mohammadfaizullah.ansari@phenompeople.com>
2018-05-09 13:20:08 +0530

    added service and ng-grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,187 @@
-import { Component } from '@angular/core';
+import { AppService } from './app.service';
+import { Component, OnInit } from '@angular/core';
+import "ag-grid-enterprise";
+import { MoodEditor } from "./mood-editor.component";
+import { MoodRenderer } from "./mood-renderer.component";
+import { NumericEditor } from "./numeric-editor.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+  }
   title = 'app';
+  jsonData;
+
+  // constructor(private _appService: AppService) { }
+
+  // ngOnInit(): void {
+  //   this._appService.getData("http://localhost:3000/getuser").then(
+  //     response => {
+  //       console.log(JSON.stringify(response));
+  //       this.jsonData = response;
+  //     },
+  //     reject => {
+  //       console.error(JSON.stringify(reject))
+
+  //     }
+  //   );
+  // }
+
+
+
+  private gridApi;
+  private gridColumnApi;
+
+  private columnDefs;
+  private rowData;
+  private frameworkComponents;
+
+  constructor() {
+    this.columnDefs = [
+      {
+        headerName: "Name",
+        field: "name",
+        width: 300,
+        editable: true,
+        cellEditor: "agRichSelect",
+        cellEditorParams: {
+          values: [
+            "Bob",
+            "Harry",
+            "Sally",
+            "Mary",
+            "John",
+            "Jack",
+            "Sue",
+            "Sean",
+            "Niall",
+            "Albert",
+            "Fred",
+            "Jenny",
+            "Larry"
+          ]
+        }
+      },
+      {
+        headerName: "Mood",
+        field: "mood",
+        cellRenderer: "moodRenderer",
+        cellEditor: "moodEditor",
+        editable: true,
+        width: 300
+      },
+      {
+        headerName: "Numeric",
+        field: "number",
+        cellEditor: "numericEditor",
+        editable: true,
+        width: 280
+      }
+    ];
+    this.rowData = createRowData();
+    this.frameworkComponents = {
+      moodRenderer: MoodRenderer,
+      moodEditor: MoodEditor,
+      numericEditor: NumericEditor
+    };
+  }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    params.api.sizeColumnsToFit();
+  }
+  onRowEditingStarted(event) {
+    console.log('never called - not doing row editing');
+  }
+  onRowEditingStopped(event) {
+    console.log('never called - not doing row editing');
+  }
+  onCellEditingStarted(event) {
+    console.log('cellEditingStarted');
+  }
+  onCellEditingStopped(event) {
+    console.log('cellEditingStopped');
+  }
+
+
+
+  updateData(event) {
+    console.log(JSON.stringify(this.rowData));
+  }
+}
+
+function createRowData() {
+  return [
+    {
+      name: "Bob",
+      mood: "Happy",
+      number: 10
+    },
+    {
+      name: "Harry",
+      mood: "Sad",
+      number: 3
+    },
+    {
+      name: "Sally",
+      mood: "Happy",
+      number: 20
+    },
+    {
+      name: "Mary",
+      mood: "Sad",
+      number: 5
+    },
+    {
+      name: "John",
+      mood: "Happy",
+      number: 15
+    },
+    {
+      name: "Jack",
+      mood: "Happy",
+      number: 25
+    },
+    {
+      name: "Sue",
+      mood: "Sad",
+      number: 43
+    },
+    {
+      name: "Sean",
+      mood: "Sad",
+      number: 1335
+    },
+    {
+      name: "Niall",
+      mood: "Happy",
+      number: 2
+    },
+    {
+      name: "Alberto",
+      mood: "Happy",
+      number: 123
+    },
+    {
+      name: "Fred",
+      mood: "Sad",
+      number: 532
+    },
+    {
+      name: "Jenny",
+      mood: "Happy",
+      number: 34
+    },
+    {
+      name: "Larry",
+      mood: "Happy",
+      number: 13
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { MoodRenderer } from './mood-renderer.component';
+import { NumericEditor } from './numeric-editor.component';
+import { MoodEditor } from './mood-editor.component';
+import { AppService } from './app.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http'
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from "@angular/forms"; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoodEditor, MoodRenderer, NumericEditor
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule,
+    AgGridModule.withComponents([MoodEditor, MoodRenderer, NumericEditor])
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
